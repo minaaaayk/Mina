4bb1a9a7bac22dec17405baa737a64b7d0be0841
minaaaayk <mina.yaghubi95@gmail.com>
2022-07-19 15:14:45 +0430

    :construction: add image header

--- components/containers/HeaderContainer.tsx
+++ components/containers/HeaderContainer.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import styled from 'styled-components';
+import bg from '../../public/bg.png'
+
 
 export const HeaderContainer:React.FC = () => {
   return (
-    <div>HeaderContainer</div>
+    <Wrapper>HeaderContainer</Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+
+    background-image: url(${bg.src});
+    background-position: center;
+    background-size: cover;
+    height: 50vh;
+    
+    @media screen and (min-width: 600px) {
+        height: 65vh;
+    }
+
+    @media screen and (min-width: 1024px) {
+        height: 95vh;
+    }
+`;
