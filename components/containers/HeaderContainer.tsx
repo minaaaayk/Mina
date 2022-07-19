fe270e35bca492640eb0db758cc119642813c028
minaaaayk <mina.yaghubi95@gmail.com>
2022-07-19 14:40:48 +0430

    :art: improve folder structure

---
+++ components/containers/HeaderContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HeaderContainer:React.FC = () => {
+  return (
+    <div>HeaderContainer</div>
+  )
+}
---
+++ components/index.ts
@@ -0,0 +1,3 @@
+export * from './base';
+export * from './containers';
+export * from './other';
