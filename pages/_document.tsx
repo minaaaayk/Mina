db807438b25e64b4a283e668b864c5b456a4532e
minaaaayk <mina.yaghubi95@gmail.com>
2022-08-11 17:23:37 +0430

    add `getInitialProps` to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,28 @@
-import { AppType } from 'next/dist/shared/lib/utils'
-import { Html, Head, Main, NextScript, DocumentProps, DocumentContext, DocumentInitialProps } from 'next/document'
+import  Document,{ Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+class MyDocument extends Document {
 
-function Document(props: DocumentProps) {
-  return (
-    <Html>
-        <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@400;500;600;700;800&display=swap" rel="stylesheet"></link>
-        </Head>
-        <body>
-            <Main />
-            <NextScript />
-        </body>
-    </Html>
-  )
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
+  }
+  
+  render() {
+    return (
+      <Html>
+          <Head>
+              <link rel="preconnect" href="https://fonts.googleapis.com"/>
+              <link rel="preconnect" href="https://fonts.gstatic.com"/>
+              <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@400;500;600;700;800&display=swap" rel="stylesheet"></link>
+          </Head>
+          <body>
+              <Main />
+              <NextScript />
+          </body>
+      </Html>
+      );
+  }
 }
-export default Document;
+
+export default MyDocument;
