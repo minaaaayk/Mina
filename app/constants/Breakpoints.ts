261f8f20f15e08ffffd3e1e12488de05d18b0241
minaaaayk <mina.yaghubi95@gmail.com>
2023-06-17 08:43:02 +0330

    add `Breakpoint` to `constant`

---
+++ app/constants/Breakpoints.ts
@@ -0,0 +1,19 @@
+export enum Breakpoint {
+    xs = '576px',
+    // => @media (min-width: 576px) { ... }
+
+    sm= '640px',
+    // => @media (min-width: 640px) { ... }
+
+    md= '768px',
+    // => @media (min-width: 768px) { ... }
+
+    lg= '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    xl= '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    xxl= '1536px',
+    // => @media (min-width: 1536px) { ... }
+};
