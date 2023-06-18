3b7459048aff36dfd2e9d2e93a7b3b64b926c0f2
minaaaayk <mina.yaghubi95@gmail.com>
2023-06-18 18:07:46 +0330

    fix style to change language button

--- app/components/custom/ChangeLocal.tsx
+++ app/components/custom/ChangeLocal.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import { Color } from '../../constants';
 
 export const ChangeLocal:React.FC = () => {
 
@@ -9,13 +11,36 @@ export const ChangeLocal:React.FC = () => {
     return locale === 'en' ? 'fa' : 'en';
   }
   return (
-    <div
+    <LanguageButton
       onClick={() => {
         router.push({ pathname, query }, asPath, { locale: getNextLocal() })
 
       }}
     >
       {getNextLocal()}
-    </div>
+    </LanguageButton>
   )
 }
+
+const LanguageButton  = styled.button`
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-size: 2rem;
+  padding: 1rem;
+  width: 4rem;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 1.5rem;
+  margin: 0 1rem;
+  justify-content: center;
+  cursor: pointer;
+  transition: all 300ms;
+  &:hover{
+    top: 1rem;
+    background-color: #ffffff3d;
+    border-radius: .5rem;
+  }
+`;
