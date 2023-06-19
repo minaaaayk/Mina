ce6f991bbde0ba4bfd4f9b45a55603da6c8140df
minaaaayk <mina.yaghubi95@gmail.com>
2023-06-19 13:38:13 +0330

    update about

--- app/components/layouts/About.tsx
+++ app/components/layouts/About.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components';
+import { Breakpoint } from '../../constants';
 
 export const About:React.FC = () => {
   return (
-    <main>
+    <Wrapper>
         this is about
-    </main>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  background-color: antiquewhite;
+  font-size: 3rem;
+  padding: 25rem 0;
+  margin-top: -10vh;
+  @media (min-width: ${Breakpoint.md}) {
+
+    margin-top: -15vh;
+  }
+
+  @media (min-width: ${Breakpoint.lg}) {
+    margin-top: -20vh;
+  }
+`;
