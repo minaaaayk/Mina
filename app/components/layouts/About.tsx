4e175cefe163a88a60f6f6f1a9bd277de504a327
minaaaayk <mina.yaghubi95@gmail.com>
2023-06-20 13:07:15 +0330

    add gradient to title

--- app/components/layouts/About.tsx
+++ app/components/layouts/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import { Breakpoint } from '../../constants';
+import { Breakpoint, Color } from '../../constants';
 import { gradientGenerator } from '../../functions';
 
 export const About:React.FC = () => {
@@ -17,6 +17,9 @@ const Wrapper = styled.section`
   font-size: 3rem;
   padding: 25rem 0;
   margin-top: -10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   @media (min-width: ${Breakpoint.md}) {
 
     margin-top: -15vh;
@@ -30,9 +33,16 @@ const Wrapper = styled.section`
 const Title = styled.h2`
   font-size: 3.5rem;
   font-family: 600;
+  text-transform: uppercase;
+  letter-spacing: .2rem;
   display: inline-block;
   background-image: ${gradientGenerator({first:"left", opacity:1})};
   -moz-background-clip: text;
   -webkit-background-clip: text;
   color: transparent;
+  transition: all 400ms;
+  cursor: default;
+  &:hover{
+    transform: scale(1.2);
+  }
 `;
