e6c9b846321d65746d4a02ed68e6424e9f5acc7b
minaaaayk <mina.yaghubi95@gmail.com>
2023-06-20 12:49:04 +0330

    add title to about

--- app/components/layouts/About.tsx
+++ app/components/layouts/About.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import styled from 'styled-components';
 import { Breakpoint } from '../../constants';
+import { gradientGenerator } from '../../functions';
 
 export const About:React.FC = () => {
   return (
     <Wrapper>
-        this is about
+        <Title>
+          this is about
+        </Title>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  background-color: antiquewhite;
   font-size: 3rem;
   padding: 25rem 0;
   margin-top: -10vh;
@@ -23,4 +25,14 @@ const Wrapper = styled.section`
   @media (min-width: ${Breakpoint.lg}) {
     margin-top: -20vh;
   }
+`;
+
+const Title = styled.h2`
+  font-size: 3.5rem;
+  font-family: 600;
+  display: inline-block;
+  background-image: ${gradientGenerator({first:"left", opacity:1})};
+  -moz-background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
 `;
