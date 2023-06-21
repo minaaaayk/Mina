81edba38180466dbb4660bff54232c9e45530ba3
minaaaayk <mina.yaghubi95@gmail.com>
2023-06-21 16:32:52 +0330

    separate `HeaderButton` from `ChangeLocal`

--- app/components/custom/ChangeLocal.tsx
+++ app/components/custom/ChangeLocal.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useRouter } from 'next/router';
-import styled from 'styled-components';
-import { Color } from '../../constants';
+import { HeaderButton } from '../modules';
 
 export const ChangeLocal:React.FC = () => {
 
@@ -11,36 +10,14 @@ export const ChangeLocal:React.FC = () => {
     return locale === 'en' ? 'fa' : 'en';
   }
   return (
-    <LanguageButton
+    <HeaderButton
       onClick={() => {
         router.push({ pathname, query }, asPath, { locale: getNextLocal() })
 
       }}
     >
       {getNextLocal()}
-    </LanguageButton>
+    </HeaderButton>
   )
 }
 
-const LanguageButton  = styled.button`
-  background-color: transparent;
-  border: none;
-  color: white;
-  font-size: 2rem;
-  padding: 1rem;
-  width: 4rem;
-  height: 4rem;
-  display: flex;
-  align-items: center;
-  position: absolute;
-  top: 1.5rem;
-  margin: 0 1rem;
-  justify-content: center;
-  cursor: pointer;
-  transition: all 300ms;
-  &:hover{
-    top: 1rem;
-    background-color: #ffffff3d;
-    border-radius: .5rem;
-  }
-`;
