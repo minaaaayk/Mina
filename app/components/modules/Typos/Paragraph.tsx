fa8c790bc8a681ac17092a815e8c7964085ae7f0
minaaaayk <mina.yaghubi95@gmail.com>
2023-08-07 08:23:19 +0330

    update typo

--- app/components/modules/Typos/Paragraph.tsx
+++ app/components/modules/Typos/Paragraph.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
 export const Paragraph = styled.p`
-  
+    font-size: 1.6rem;
+    &:not(:last-child) {
+        margin-bottom: 3rem;
+    }
 `;
--- app/components/modules/Typos/TertiaryHeading.tsx
+++ app/components/modules/Typos/TertiaryHeading.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
 export const TertiaryHeading = styled.h3`
-  
+    font-size: 1.6rem;
+    font-weight: 700;
+    text-transform: uppercase;
 `;
