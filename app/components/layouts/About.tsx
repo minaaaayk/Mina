821b90dcc8e1a86c34dae0082d4d432a7097faba
minaaaayk <mina.yaghubi95@gmail.com>
2023-08-07 10:29:27 +0330

    add `Lorem Ipsum`

--- app/components/layouts/About.tsx
+++ app/components/layouts/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import { Breakpoint } from '../../constants';
-import { SecondaryHeading } from '../modules';
+import { Paragraph, SecondaryHeading, TertiaryHeading } from '../modules';
 
 export const About:React.FC = () => {
   return (
@@ -9,6 +9,15 @@ export const About:React.FC = () => {
         <SecondaryHeading> this is about </SecondaryHeading>
         <GridWrapper>
           <DescriptionWrapper>
+            <TertiaryHeading> What is Lorem Ipsum? </TertiaryHeading>
+            <Paragraph>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.ets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </Paragraph>
+
+            <TertiaryHeading> Why do we use it? </TertiaryHeading>
+            <Paragraph>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </Paragraph>
 
           </DescriptionWrapper>
           <ImagesWrapper>
@@ -39,8 +48,12 @@ const Wrapper = styled.section`
 
 const GridWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 2rem;
+  grid-template-columns: 1fr;
+  @media (min-width: ${Breakpoint.md}) {
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+  }
+  padding: 10rem;
 `;
 const DescriptionWrapper = styled.div`
 `;
