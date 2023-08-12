1cf2aef7343edc5cb2b8d028be47cd6dda7d1c49
minaaaayk <mina.yaghubi95@gmail.com>
2023-08-12 15:39:31 +0330

    use `next/image` for header

--- app/components/layouts/Header.tsx
+++ app/components/layouts/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import { gradientGenerator } from '../../functions'
 import bg from '../../../public/bg.jpg';
+import Image from 'next/image';
 import { useTranslation } from "next-i18next";
 import { ChangeLocal } from '../custom/ChangeLocal';
 import { useIsRtl } from '../../hooks/useIsRtl';
@@ -14,37 +15,60 @@ export const Header:React.FC = () => {
   const isRtl = useIsRtl();
   
   return (
+    <>
     <Wrapper isRtl={isRtl}>
-      <ChangeLocal/>
-      <MainHeader>
-        <PrimaryHeading isRtl={isRtl}> {t("primaryTitle")}</PrimaryHeading>
-        <SubPrimaryHeading isRtl={isRtl}>{t("subTitle")}</SubPrimaryHeading>
-        <WhiteAnimatedButton href="#">{t("mainButton")}</WhiteAnimatedButton>
-      </MainHeader>
+      <GrWrapper>
+        <ChangeLocal/>
+        <MainHeader>
+          <PrimaryHeading isRtl={isRtl}> {t("primaryTitle")}</PrimaryHeading>
+          <SubPrimaryHeading isRtl={isRtl}>{t("subTitle")}</SubPrimaryHeading>
+          <WhiteAnimatedButton href="#">{t("mainButton")}</WhiteAnimatedButton>
+        </MainHeader>
+      </GrWrapper>
+      <BgImage
+        src={bg}
+        alt=""
+        objectFit="cover"
+        layout='fill'
+        objectPosition="center"
+          
+      />
     </Wrapper>
+    </>
   )
 }
 
-const Wrapper = styled.section<{isRtl: boolean}>`
-    background-image: ${gradientGenerator({first:"right", second:"top", opacity:.8})}, url(${bg.src});
-    background-position: center;
-    background-size: cover;
-    height: 50vh;
-    clip-path: ${props => 
-                    props.isRtl
-                    ? `polygon(0 0, 100% 0, 100% 100%, 0 80%)` 
-                    : 'polygon(0 0, 100% 0, 100% 80%, 0 100%)'
-                };
-    position: relative;
+const Wrapper = styled.div<{isRtl: boolean}>`
+  width: 100%;
+  position: relative;
 
-    
-    @media (min-width: ${Breakpoint.md}) {
-        height: 65vh;
-    }
+  height: 50vh;
+  @media (min-width: ${Breakpoint.md}) {
+      height: 65vh;
+  }
+  
+  @media (min-width: ${Breakpoint.lg}) {
+      height: 95vh !important;
+  }
 
-    @media (min-width: ${Breakpoint.lg}) {
-        height: 95vh;
-    }
+  clip-path: ${props => 
+        props.isRtl
+        ? `polygon(0 0, 100% 0, 100% 100%, 0 80%)` 
+        : 'polygon(0 0, 100% 0, 100% 80%, 0 100%)'
+    };
+`;
+const GrWrapper = styled.div`
+  position: absolute;
+  z-index: 1;
+  height: 100%;
+  width:100%;
+  background-position: center;
+  background-size: cover;
+  background-image: ${gradientGenerator({first:"right", second:"top", opacity:.8})};
+  
+`;
+const BgImage = styled(Image)`
+  z-index: 0;  
 `;
 
 const MainHeader = styled.div`
