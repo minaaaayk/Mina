704d34d30be5864c8e9e0bce64596572822c15a4
minaaaayk <mina.yaghubi95@gmail.com>
2023-08-15 10:11:56 +0330

    remove font icons packages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ class MyDocument extends Document {
           <Head>
               <link rel="preconnect" href="https://fonts.googleapis.com"/>
               <link rel="preconnect" href="https://fonts.gstatic.com"/>
+              <script src="https://kit.fontawesome.com/60810ea434.js" crossOrigin="anonymous" async></script>
               <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@400;500;600;700;800&display=swap" rel="stylesheet"></link>
           </Head>
           <body>
