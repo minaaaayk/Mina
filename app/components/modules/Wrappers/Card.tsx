f553077882b683589f5c1c116d48256c39501446
minaaaayk <mina.yaghubi95@gmail.com>
2023-08-15 10:23:09 +0330

    update stylse

--- app/components/modules/Wrappers/Card.tsx
+++ app/components/modules/Wrappers/Card.tsx
@@ -40,7 +40,13 @@ const Wrapper = styled.div`
     padding: 2.5rem;
     text-align: center;
     border-radius: 3px;
-    box-shadow: 0 1.5 4rem rgba(0, 0, 0, .15);
+    box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, .15);
+    transition: all .2s linear;
+    cursor: default;
+    &:hover{
+        transform: translateY(-1.5rem) scale(1.04);
+        box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, .3);
+    }
 `;
 
 
