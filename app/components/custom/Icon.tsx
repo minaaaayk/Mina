735bca516ce19342464be495a2a23d025ade9f8e
minaaaayk <mina.yaghubi95@gmail.com>
2023-08-15 10:48:27 +0330

    add custom Icon

---
+++ app/components/custom/Icon.tsx
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+import { IconProp } from "../../types";
+
+export const Icon = styled.i<{className: IconProp}>``;
--- app/components/custom/index.ts
+++ app/components/custom/index.ts
@@ -1,2 +1,3 @@
 export * from './ChangeLocal';
 export * from './Card';
+export * from './Icon';
