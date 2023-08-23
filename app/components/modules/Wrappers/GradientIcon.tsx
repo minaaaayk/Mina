e58ee4f288bc9c3c02ab9f7899e5d2a106f91c69
minaaaayk <mina.yaghubi95@gmail.com>
2023-08-23 17:43:00 +0330

    improve GradientIcon

--- app/components/modules/Wrappers/GradientIcon.tsx
+++ app/components/modules/Wrappers/GradientIcon.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const GradientIcon = styled.i`
   font-size: 6rem;
   display: inline-block;
-  background-image: linear-gradient(90deg, ${Color.gradientBlue1} 0%, ${Color.gradientPurple3} 55%, ${Color.gradientPink1} 100%);
+  background-image: linear-gradient(90deg, ${Color.gradientBlue1} 0%, ${Color.gradientPurple3} 100%);
   -moz-background-clip: text;
   background-clip: text;
   -webkit-background-clip: text;
